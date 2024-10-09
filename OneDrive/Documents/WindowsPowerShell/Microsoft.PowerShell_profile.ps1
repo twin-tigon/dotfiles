b76f735af986a0deb59bf9e4bb60805410e8e5aa
Rodrigo García <yo@rodrigogarcia.me>
2024-10-08 20:21:55 -0600

    Add Chocolatey tab completion

--- OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,13 @@ New-Alias -Name g -Value git.exe
 New-Alias -Name code -Value code-insiders.exe
 New-Alias -Name dotfiles -Value Invoke-DotfilesGit
 
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
